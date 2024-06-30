43f20869fccec71a43ecef6439044cee26fe430b
jimmyboy722 <jeckman722@gmail.com>
2024-06-30 15:39:25 -0400

    Added function to handle adding a new task and adding tasks to task list to save and render

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,18 @@ let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
-function generateTaskId() {}
+// also manages task's individual unique identifiers
+// So if nextId isn't created, it initializes it with value "1". Otherwise nextId is incremented by 1.
+// After updating, thr new value is stored in localStorage under the nextID key and returns the updated nextID.
+function generateTaskId() {
+  if (nextId === null) {
+    nextId = 1;
+  } else {
+    nextId++;
+  }
+  localStorage.setItem("nextID", JSON.stringify(nextId));
+  return nextId;
+}
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
@@ -14,7 +25,26 @@ function createTaskCard(task) {
 function renderTaskList() {}
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event) {}
+function handleAddTask(event) {
+  // to prevent default action of event lister before task is created
+  event.preventDefault();
+  // Task object
+  const newTask = {
+    id: generateTaskId(),
+    taskTitle: $("#taskTitle").val(),
+    description, $('#taskDescription').val(),
+    dueDate, $('#datePicker').val(),
+    status: 'To Do',
+  };
+// For adding new tasks to taskList, saving and rendering on screen
+taskList.push(newTask);
+localStorage.setItem('newTasks', JSON.stringify(taskList));
+renderTaskList();
+$('taskTitle').val('');
+$('description').val('');
+$('dueDate').val('');
+
+}
 
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event) {}
