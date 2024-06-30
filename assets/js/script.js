e209dbad7fbcd6e17c9fde86bacefb800efb00cc
jimmyboy722 <jeckman722@gmail.com>
2024-06-30 14:40:32 -0400

    Added styling and date picker to modal form.

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,3 +24,10 @@ function handleDrop(event, ui) {}
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {});
+
+$(function () {
+  $("#datePicker").datepicker();
+  $("#anim").on("change", function () {
+    $("#datePicker").datepicker("option", "showAnim", $(this).val());
+  });
+});
